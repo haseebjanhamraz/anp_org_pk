3ef5783c848b11a4b4192f5189908af34f7b4f94
haseebjanhamraz <haseebjanhamraz@gmail.com>
2024-11-14 02:48:11 +0500

    Update landing page little bit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
 import Hero from "./components/Hero";
 import BachaKhanQuotes from "./components/website_sections/BachaKhanQuotes";
+import Downloads from "./components/Downloads";
+import LeaderCard from "./components/LeaderCard";
+import Divider from "@mui/material/Divider";
+
 export default function Home() {
   return (
     <div>
       <Hero />
       <BachaKhanQuotes />
+      <div className="flex flex-col md:flex-row mt-10 gap-10 justify-center">
+        <div className="px-4 md:px-0">
+          <h1 className="text-2xl font-bold text-center md:text-left">Downloads</h1>
+          <Divider className="my-2" />
+          <Downloads />
+        </div>
+        <div className="px-4 md:px-0">
+          <h1 className="text-2xl ml-10 font-bold text-center md:text-left">Leadership</h1>
+          <Divider className="my-2" />
+          <LeaderCard />
+        </div>
+      </div>
     </div>
   );
 }
