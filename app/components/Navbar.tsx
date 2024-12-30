6672bad60358a07f14923eb2ee7ca44ace8cfcc9
haseebjanhamraz <haseebjanhamraz@gmail.com>
2024-12-30 22:46:14 +0500

    feat: update Navbar component for improved mobile responsiveness and layout adjustments

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,29 +15,35 @@ export const Navbar = () => {
     const user = useUser();
 
     useEffect(() => {
-        const handleResize = () => setIsMobile(window.innerWidth < 1100);
+        const handleResize = () => setIsMobile(window.innerWidth < 1020);
         handleResize();
         window.addEventListener("resize", handleResize);
         return () => window.removeEventListener("resize", handleResize);
     }, []);
 
     return (
-        <nav className="flex sticky z-50 top-0 flex-wrap items-center justify-between p-3 px-8 bg-gray-100 dark:bg-slate-800">
+        <nav className="flex md:sticky z-50 top-0 flex-wrap items-center justify-between p-3 px-8 bg-gray-100 dark:bg-slate-800">
             {/* Logo Section */}
             <Link href="/" className="flex items-center gap-4">
                 <Image src="/anp-logo.png" width={70} height={70} alt="ANP Logo" />
                 <div className="text-center">
-                    <h4 className="lg:text-2xl md:text-xl font-extrabold uppercase text-red-500">
-                        Awami National Party
-                    </h4>
-                    <span className="lg:text-md md:text-sm text-gray-400 dark:text-red-800 font-light">
-                        PEACE - DEMOCRACY - DEVELOPMENT
-                    </span>
+                    {!isMobile ? (
+                        <>
+
+                            <h4 className="text-xl font-extrabold uppercase text-red-500">
+                                Awami National Party
+                            </h4>
+                            <span className="lg:text-md md:text-sm text-gray-400 dark:text-red-800 font-light">
+                                PEACE - DEMOCRACY - DEVELOPMENT
+                            </span>
+                        </>
+                    ) : null}
                 </div>
             </Link>
 
             {/* Menu Section */}
-            <div className="flex items-center gap-4">
+            <div className="flex items-center justify-center gap-4">
+
                 {isMobile ? (
                     <MobileMenu />
                 ) : (
@@ -54,18 +60,7 @@ export const Navbar = () => {
                         <DesktopMenu />
                     </Box>
                 )}
-
                 <DarkModeToggle />
-
-                {/* Dashboard Link for Admin */}
-                {user?.role === "admin" && (
-                    <Link href="/dashboard" className="flex items-center gap-2">
-                        <MdDashboard className="text-2xl text-red-500 dark:text-red-800" />
-                        <span className="text-sm font-bold text-red-500 dark:text-red-800">
-                            Dashboard
-                        </span>
-                    </Link>
-                )}
             </div>
         </nav>
     );
