30def20f68af995134eeb7b0fef0fc0dbacc54af
haseebjanhamraz <haseebjanhamraz@gmail.com>
2025-01-13 17:43:47 +0500

    Update base URL in robots.txt and format code for consistency

--- app/robots.ts
+++ app/robots.ts
@@ -1,14 +1,21 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = "https://anp.org.pk";
+  const baseUrl = "https://awaminationalparty.org";
 
-    return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: ['/dashboard/', '/auth/', '/api/', '/_next/', '/fonts/', '/images/']
-        },
-        sitemap: `${baseUrl}/sitemap.xml`,
-    }
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/dashboard/",
+        "/auth/",
+        "/api/",
+        "/_next/",
+        "/fonts/",
+        "/images/",
+      ],
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
 }
